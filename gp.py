8efa2247c70fb2a9194a257e5f462a21222cec12
Anirudh Ajith <anirudh.ajith@gmail.com>
2020-06-23 16:41:35 +0530

    Re-engineered interface: Now takes secret string as user input. Removed config.json. Added command line options, switches.

--- gp.py
+++ gp.py
@@ -1,36 +1,70 @@
-import sys
+import argparse
 from hashlib import sha256
-import json
+import getpass 
+import random
 
-def generateHash(platform, configFile = 'config.json'):
-    with open(configFile) as f:
-        data = json.load(f)
-        secret = data["secret"]
-        length = data["length"]
-        if (length < 5 or length > 64):
-            raise ValueError("Invalid length")
-
-    raw = secret["str1"] + " " + str(sys.argv[1]).lower() + " " + secret["str2"] + " " + str(length)
-    hash = sha256(raw.encode('utf-8')).hexdigest()
-    chopped = hash[:length]
-
+def generateHash(platform, secretString, length):
+    raw = list(secretString + ' ' + str(platform).lower() + ' ' + str(length))
+    random.Random(length).shuffle(raw)
+    shuffled = ''.join(raw)
+    
+    hashed = sha256(shuffled.encode('utf-8')).hexdigest()
+    chopped = hashed[:length]
     return chopped
 
-def capitaliseAndSpecialChar(hash, specialChar = '!'):
-    hash = list(hash)
-    for i in range(len(hash)):
-        c = hash[i]
+def capitaliseAndSpecialChar(hashed, specialChar = '!'):
+    hashed = list(hashed)
+    hashed[-1] = specialChar
+    
+    for i in range(len(hashed)):
+        c = hashed[i]
         if c.islower():
-            hash[i] = c.upper()
+            hashed[i] = c.upper()
             break
-    hash[-1] = specialChar
-    return ''.join(hash)
+    
+    return ''.join(hashed)
+
+def getPassword(platform, secretString, length):
+    hashed = generateHash(platform, secretString, length)
+    password = capitaliseAndSpecialChar(hashed)
+    return password
 
 if __name__ == "__main__":
-    n = len(sys.argv) 
-    if n != 2:
-        raise ValueError("Invalid arguments")
+    
+    parser = argparse.ArgumentParser(description='Generate Password: Personal multi-platform password generation scheme')
+    parser.add_argument('platform', type=str, help='name of platform for which password should be generated', nargs='?', metavar='PLATFORM')
+    parser.add_argument('--list', type=str, help='path to file containing all platforms', metavar='PATH')
+    parser.add_argument('--length', type=int, help='required length of generated password', metavar='LENGTH')
+    args = parser.parse_args()
 
-    hash = generateHash(sys.argv[1])
-    password = capitaliseAndSpecialChar(hash)
-    print(password)
+    if args.platform == None and args.list == None:
+        raise ValueError('Invalid usage. Use --help for more information.')
+    elif args.platform != None and args.list != None:
+        raise ValueError('Invalid usage. Use --help for more information.')
+    elif args.platform == '' or args.list == '':
+        raise ValueError('Invalid usage. Use --help for more information.')
+    
+    length = 16 if args.length == None else args.length
+    if length < 5 or length > 64:
+        raise ValueError('Invalid length')
+    
+    
+    if args.list == None:
+        secretString = getpass.getpass(prompt='Enter your secret string: ')
+        print()
+
+        platform = args.platform.lower()
+        password = getPassword(platform, secretString, length)
+        print ("{:<20} {:<64}".format(platform + ':', password))
+    else: 
+        with open(args.list) as f:
+            platforms = f.readlines()
+            platforms = [platform.rstrip().lower() for platform in platforms]
+            platforms = [platform for platform in platforms if platform]
+        
+        secretString = getpass.getpass(prompt='Enter your secret string: ')
+        print()
+
+        for platform in platforms:
+            password = getPassword(platform, secretString, length)
+            print ("{:<20} {:<64}".format(platform + ':', password)) 
